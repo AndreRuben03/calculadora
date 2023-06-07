9812bde56a9603eef60758fce9eb76a45f1fe0ba
André Rúben <andreruben03@gmail.com>
2023-06-07 16:51:53 -0300

    correções no js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,55 @@
+function adicao(n1, n2) {
+    
+    let = n1
+    let = n2
+    let resultado = document.getElementById('resultado').value;
+    
+    switch(resultado) {
+        case '+':
+            document.getElementById('resultado').innerHTML =  (n1 + n2)
+            break
+    }
+}
+
+function subtracao(n1, n2) {
+    
+    let = n1
+    let = n2
+    let resultado = document.getElementById('resultado').value;
+    
+    switch(resultado) {
+        case '-':
+            document.getElementById('resultado').innerHTML = (n1 - n2)
+            break
+    }
+}
+
+function divisao(n1, n2) {
+    
+    let = n1
+    let = n2
+    let resultado = document.getElementById('resultado').value;
+    
+    switch(resultado) {
+        case  '/':
+            document.getElementById('resultado').innerHTML = (n1 / n2)
+            break
+    }
+}
+
+function multiplicacao(n1, n2) {
+    
+    let = n1
+    let = n2
+    let resultado = document.getElementById('resultado').value;
+    
+    switch(resultado) {
+        case  '*':
+            document.getElementById('resultado').innerHTML (n1 * n2)
+            break
+    }
+}
+
 function calculadora() {
 
     let n1 = parseInt(document.getElementById('n1').value);
@@ -19,4 +71,3 @@ function calculadora() {
             break
     }
 }
-
